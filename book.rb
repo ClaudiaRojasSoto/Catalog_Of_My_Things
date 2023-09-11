af388b2af472fbcf60c88b728f2582c388ff9197
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-09-11 19:06:55 -0300

    Change the method can be archived to private

--- book.rb
+++ book.rb
@@ -2,14 +2,15 @@ require_relative 'item'
 
 # Class representing a Book
 class Book < Item
-  attr_accessor :publisher, :cover_state, :label
+  attr_accessor :publisher, :cover_state
 
   def initialize(params = {})
     super(params)
     @publisher = params[:publisher]
     @cover_state = params[:cover_state]
-    @label = params[:label]
   end
+
+  private
 
   def can_be_archived?
     super || @cover_state == 'bad'
