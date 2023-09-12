5326e5324c84d23b01c2fcd4a27f73936399200b
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-09-12 00:14:17 -0300

    Upload the item class making the id accesible for the subclases as a book or games

--- item.rb
+++ item.rb
@@ -1,14 +1,15 @@
+require 'date'
+
+# Class representing an Item
 class Item
-  attr_accessor :genre, :author, :label, :publish_date
+  attr_accessor :id, :genre, :author, :label, :publish_date
 
   def initialize(params = {})
-    self.class.id_counter = (self.class.id_counter || 0) + 1
+    @id = Random.rand(1..10_000)
     @genre = params[:genre]
     @author = params[:author]
     @label = params[:label]
     @publish_date = (Date.strptime(params[:publish_date], '%d-%m-%Y') if params[:publish_date])
-    private
-    @id = self.class.id_counter
     @archived = false
   end
 
@@ -16,19 +17,16 @@ class Item
     @archived = true if can_be_archived?
   end
 
-  # Method that adds genre and updates the reference in the genre object
   def add_genre(genre)
     @genre = genre
     genre.items.push(self) unless genre.items.include?(self)
   end
 
-  # Method that adds author and updates the reference in the author object
   def add_author(author)
     @author = author
     author.items.push(self) unless author.items.include?(self)
   end
 
-  # Method that adds label and updates the reference in the label object
   def add_label(label)
     @label = label
     label.items.push(self) unless label.items.include?(self)
@@ -36,7 +34,7 @@ class Item
 
   private
 
-  attr_reader :id, :archived
+  attr_reader :archived
 
   def can_be_archived?
     Time.now.year - @publish_date.year > 10
