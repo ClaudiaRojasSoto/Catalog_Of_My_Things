5b3102b05cad8c95a879e61b590b5cd4d0386712
francksefu <90141102+francksefu@users.noreply.github.com>
2023-09-13 21:48:21 +0200

    Update schema.sql file

--- schema.sql
+++ schema.sql
@@ -4,33 +4,21 @@ CREATE TABLE Label (
     color VARCHAR(20)
 );
 
-<--CREATE TABLE Source (
-<--    id INT PRIMARY KEY AUTO_INCREMENT,
-<--    name VARCHAR(255)
-<--);
-
-<--CREATE TABLE Genre (
-<--    id INT PRIMARY KEY AUTO_INCREMENT,
-<--    name VARCHAR(255)
-<--);
-
-<--CREATE TABLE Author (
-<--    id INT PRIMARY KEY AUTO_INCREMENT,
-<--    first_name VARCHAR(255),
-<--    last_name VARCHAR(255)
-<--);
+CREATE TABLE Genre (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255)
+);
 
 CREATE TABLE Item (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    <-- genre_id INT,
-    <--author_id INT,
+     genre_id INT,
+    author_id INT,
     <--source_id INT,
     label_id INT,
     publish_date DATE,
     archived BOOLEAN,
-    <--FOREIGN KEY (genre_id) REFERENCES Genre(id),
-    <--FOREIGN KEY (author_id) REFERENCES Author(id),
-    <--FOREIGN KEY (source_id) REFERENCES Source(id),
+    FOREIGN KEY (genre_id) REFERENCES Genre(id),
+    FOREIGN KEY (author_id) REFERENCES Author(id),
     FOREIGN KEY (label_id) REFERENCES Label(id)
 );
 
@@ -41,17 +29,11 @@ CREATE TABLE Book (
     FOREIGN KEY (item_id) REFERENCES Item(id)
 );
 
-<--CREATE TABLE MusicAlbum (
-<--    item_id INT PRIMARY KEY,
-<--    on_spotify BOOLEAN,
-<--    FOREIGN KEY (item_id) REFERENCES Item(id)
-<--);
-
-<--CREATE TABLE Movie (
-<--    item_id INT PRIMARY KEY,
-<--    silent BOOLEAN,
-<--    FOREIGN KEY (item_id) REFERENCES Item(id)
-<--);
+CREATE TABLE MusicAlbum (
+    item_id INT PRIMARY KEY,
+    on_spotify BOOLEAN,
+    FOREIGN KEY (item_id) REFERENCES Item(id)
+);
 
 CREATE TABLE Games (
     id INT GENERATED ALWAYS AS IDENTITY,
