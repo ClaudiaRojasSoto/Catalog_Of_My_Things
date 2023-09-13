354e57da644a5185ff98669c10bde5b3ab3f98a3
francksefu <90141102+francksefu@users.noreply.github.com>
2023-09-13 09:06:22 +0200

    Use mock and correct linters error

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,21 +1,21 @@
 require_relative '../game'
 require_relative '../author'
+require 'rspec'
 
-describe Game do
-
+RSpec.describe Game do
   describe '#can_be_archived?' do
     context 'when last_played_at is greater than 2 years' do
       it 'returns return true' do
-        @author = Author.new('franck', 'sefu')
-        @game = Game.new(true, '12-12-2000', '12-12-2001', @author)
+        author_mock = instance_double(Author, first_name: 'franck', last_name: 'sefu')
+        @game = Game.new(true, '12-12-2000', '12-12-2001', author_mock)
         expect(@game.send(:can_be_archived?)).to eq(true)
       end
     end
 
     context 'when last_played_at is less than 2 years' do
       it 'returns false' do
-        @author = Author.new('franck', 'sefu')
-        @game = Game.new(true, '12-12-2023', '12-12-2023', @author)
+        author_mock = instance_double(Author, first_name: 'Guy', last_name: 'sefu')
+        @game = Game.new(true, '12-12-2023', '12-12-2023', author_mock)
         expect(@game.send(:can_be_archived?)).to eq(false)
       end
     end
