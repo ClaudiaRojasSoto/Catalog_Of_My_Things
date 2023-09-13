e4c026ab8e80b06d08a59c591730a402b6af7382
francksefu <90141102+francksefu@users.noreply.github.com>
2023-09-13 08:41:25 +0200

    Fix Error

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,17 +1,21 @@
 require_relative '../game'
+require_relative '../author'
 
 describe Game do
+
   describe '#can_be_archived?' do
     context 'when last_played_at is greater than 2 years' do
       it 'returns return true' do
-        @game = Game.new(true, '12-12-2000', '12-12-2001')
+        @author = Author.new('franck', 'sefu')
+        @game = Game.new(true, '12-12-2000', '12-12-2001', @author)
         expect(@game.send(:can_be_archived?)).to eq(true)
       end
     end
 
     context 'when last_played_at is less than 2 years' do
       it 'returns false' do
-        @game = Game.new(true, '12-12-2023', '12-12-2023')
+        @author = Author.new('franck', 'sefu')
+        @game = Game.new(true, '12-12-2023', '12-12-2023', @author)
         expect(@game.send(:can_be_archived?)).to eq(false)
       end
     end
