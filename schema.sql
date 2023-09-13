dbb01dc5b272971c32ea2e37fba36de33ab54cea
francksefu <90141102+francksefu@users.noreply.github.com>
2023-09-13 21:24:53 +0200

    Update schema.sql file by adding table for game and author

--- schema.sql
+++ schema.sql
@@ -53,9 +53,19 @@ CREATE TABLE Book (
 <--    FOREIGN KEY (item_id) REFERENCES Item(id)
 <--);
 
-<--CREATE TABLE Game (
-<--item_id INT PRIMARY KEY,
-<--multiplayer BOOLEAN,
-<--last_played_at DATE,
-<--FOREIGN KEY (item_id) REFERENCES Item(id)
-<--);
+CREATE TABLE Games (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    multiplayer BOOLEAN NOT NULL,
+    last_payed_at DATE NOT NULL,
+    published_date DATE NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Author (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    items INTEGER,
+    PRIMARY KEY (id),
+    FOREIGN KEY (items) REFERENCES Games(id)
+);
